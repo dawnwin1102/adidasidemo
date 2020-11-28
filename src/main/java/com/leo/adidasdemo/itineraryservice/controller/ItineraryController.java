4098423d2a698c49bd7e436324a76e11e957f821
Xiaodong Mao <v-xiaomao@microsoft.com>
2020-11-28 14:08:39 +0800

    update

--- src/main/java/com/leo/adidasdemo/itineraryservice/controller/ItineraryController.java
+++ src/main/java/com/leo/adidasdemo/itineraryservice/controller/ItineraryController.java
@@ -1,23 +1,23 @@
 package com.leo.adidasdemo.itineraryservice.controller;
 
-import com.leo.adidasdemo.itineraryservice.dao.RouteRepository;
-import com.leo.adidasdemo.itineraryservice.entities.Route;
 import com.leo.adidasdemo.itineraryservice.service.PathService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
+@RestController()
+@RequestMapping("itinerary")
 public class ItineraryController {
     @Autowired
     private PathService pathService;
+
     @GetMapping("/getShortestPath")
-    public List<String> getShortestPath(@RequestParam(value = "start",required = false) String start, @RequestParam(value = "end",required = false) String end) {
-        List<String> res=pathService.getShorestRoute(start,end);
+    public List<String> getShortestPath(@RequestParam(value = "start", required = false) String start, @RequestParam(value = "end", required = false) String end) {
+        List<String> res = pathService.getShorestRoute(start, end);
         return res;
     }
 }
